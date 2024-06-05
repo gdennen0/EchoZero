7152f760c88b028624bb8c2e32882509b3533173
codacodacoda <111922569+codacodacoda@users.noreply.github.com>
2024-06-05 12:12:06 -0500

    Began outlining application data control flow in ideas.txt Wrote initial version of load_audio.py... still in progress and has not been debugged

---
+++ Model/load_audio.py
@@ -0,0 +1,36 @@
+import torchaudio
+import torchaudio.Transform as transforms
+
+# ingest_audio class loads & standardizes the audio file's samplerate
+# Args AUDIO_FILE should be the file location, and the TARGET_SAMPLERATE should be the constant for the target samplerate
+
+class load_audio(AUDIO_FILE, TARGET_SAMPLERATE):
+    def __init__(self):
+        print("CLASS: inget_audio: __init__")
+        self.audio_file = AUDIO_FILE
+        self.target_samplerate = TARGET_SAMPLERATE
+        self.samplerate = None
+        self.waveform = None
+
+    def load_audio(self):
+        self.waveform, self.samplerate = torchaudio.load(AUDIO_FILE)
+        print(f"Loaded Audio Waveform: {self.waveform}")
+        print(f"LOaded Audio Samplerate: {self.samplerate}")
+
+    def standardize_samplerate(self):
+        rate_transform = transforms.Resample(self.samplerate, self.target_samplerate)
+        resampled_waveform = rate_transform(self.waveform)
+        return resampled_waveform
+
+    def standardize_samplerate_logic(self):
+        if self.samplerate > self.target_samplerate:
+            print(f"Target Sample Rate ({self.target_samplerate}) is less than current sample rate ({self.samplerate})")
+            self.waveform = self.standardize_samplerate(self.waveform)
+            return self.waveform
+        elif self.samplerate < self.target_samplerate:
+            print(f"Target Sample Rate ({self.target_samplerate}) is greater than current sample rate ({self.samplerate})")
+            self.waveform = self.standardize_samplerate(self.waveform)
+            return self.waveform
+        elif self.samplerate == self.target_samplerate:
+            print(f"Target Sample Rate ({self.target_samplerate}) equals current sample rate ({self.samplerate})")
+            pass
