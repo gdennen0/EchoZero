9a321272a27373829d4e72ba535d6cdd4eef2e7f
gdennen <griffindennen@gmail.com>
2024-06-05 16:49:28 -0700

    added some extra comments

--- Control/ControlLanguage.py
+++
@@ -1,9 +0,0 @@
-# Load all of the control language modules
-
-function load_objects()
-function load_functions()
-function load_options()
-
-
-control = {}
-
--- Control/Function/controller.py
+++ Control/Function/controller.py
@@ -1,12 +1,26 @@
-# Main Controller Function
+from Model.tools import prompt, path_exists, file_exists, Log, is_valid_audio_format
+from Model.load_audio import load_audio
+# Main Controller Functions
 
-
-class ingest:
+class Ingest:
     # Get user input
+    path = prompt("Please Enter Path:")
     # validity check
-    pass
-
+    if not path_exists(path): # Check if the path is valid
+        Log.error(f"Invalid Path: '{path}'")
+        pass
+    if not file_exists(path):   # Check if the file exists at specified path
+        Log.error(f"File does not exist at specified path")
+        pass
+    if not is_valid_audio_format(path): # Check if audio is in a usable format
+        Log.error(f"Invalid audio format")
+        pass
+    
+    # Load the audio into the model  
+    
 class Digest:
     def process_data(self):
         # Placeholder for digest data process implementation
         pass
+
+    
