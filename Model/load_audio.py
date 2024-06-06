1566c0189c8fc21630992ba1378f518d22c7db1f
Griffin D <gdennen@G-MBP.localdomain>
2024-06-06 14:53:31 -0700

    Working on load_audio functionality

--- Model/load_audio.py
+++ Model/load_audio.py
@@ -1,22 +1,47 @@
+import librosa
 import torchaudio
 import torchaudio.Transform as transforms
+from Model.audio import audio
+from Model.tools import Log
 
 # ingest_audio class loads & standardizes the audio file's samplerate
 # Args AUDIO_FILE should be the file location, and the TARGET_SAMPLERATE should be the constant for the target samplerate
 
+TARGET_SAMPLERATE = 44100
+FRAMERATE = 30
 class load_audio:
-    def __init__(self, AUDIO_FILE, TARGET_SAMPLERATE):
+    def __init__(self, audio_path, target_sr=TARGET_SAMPLERATE):
         print("CLASS: inget_audio: __init__")
-        self.audio_file = AUDIO_FILE
-        self.target_samplerate = TARGET_SAMPLERATE
-        self.samplerate = None
-        self.waveform = None
+        self.load_audio(audio_path, target_sr)
 
-    def load_audio(self):
-        self.waveform, self.samplerate = torchaudio.load(self.audio_file)
-        print(f"Loaded Audio Waveform: {self.waveform}")
-        print(f"Loaded Audio Samplerate: {self.samplerate}")
-        # Create a song object 
+    def load_audio(self, audio_path, target_sr):
+        # Loads all necessary audio data
+        audio_data, _ = self.create_audio_data(audio_path, target_sr)   # creates audio data array using librosa
+        t, _ = self.create_audio_tensor(audio_path, target_sr)  # creates a tensor object with the audio file
+        a = self.create_audio_object(audio_data, t, target_sr, FRAMERATE, None, "Default")  # creates an audio object and updates the necessary data
+
+    def create_audio_data(self, audio_path, target_sr):
+        # creates audio data array using librosa
+        data, sr = librosa.load(audio_path, sr=target_sr)
+        return data, sr
+    
+    def create_audio_tensor(audio_path, target_sr):
+        # creates a tensor object with the audio file
+        # standardise sample rate??
+        audio_tensor, sr = torchaudio.load(audio_path, target_sr)
+        return audio_tensor, sr
+
+    def create_audio_object(self, data, tensor, sr, fps, type, name):
+        # creates an audio object and updates the necessary data
+        a = audio()
+        a.set_original_data(data)
+        a.set_tensor(tensor)
+        a.set_sample_rate(sr)
+        a.set_frame_rate(fps)
+        a.set_type(type)
+        a.set_name(name)
+        return a
+    
     def standardize_samplerate(self):
         rate_transform = transforms.Resample(self.samplerate, self.target_samplerate)
         resampled_waveform = rate_transform(self.waveform)
