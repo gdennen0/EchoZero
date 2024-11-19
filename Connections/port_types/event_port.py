fbd86aca7d7cede6311ab589ff0882b9223c2799
codacodacoda <111922569+codacodacoda@users.noreply.github.com>
2024-11-19 12:23:24 -0600

    Updated event_port and generic_container files for export.py

--- Connections/port_types/event_port.py
+++ Connections/port_types/event_port.py
@@ -2,8 +2,8 @@ from .port import Port
 from DataTypes.event_data_type import EventData
 from tools import Log
 
-class AudioPort(Port):
-    name = "AudioPort"
+class EventPort(Port):
+    name = "EventPort"
 
     def __init__(self):
         super().__init__()
--- Container/ContainerTypes/generic_container.py
+++ Container/ContainerTypes/generic_container.py
@@ -5,6 +5,7 @@ from Block.BlockTypes.Load.LoadAudioBlock import LoadAudioBlock
 from Block.BlockTypes.Transform.TransformAudioBlock import TransformAudioBlock 
 from Block.BlockTypes.Export.export_audio import ExportAudioBlock
 from Block.BlockTypes.Export.export_audio_spectrogram import ExportAudioSpectrogramBlock
+from Block.BlockTypes.Export.export_ma3 import ExportMA3Block
 class GenericContainer(Container):
     def __init__(self):
         super().__init__()
@@ -15,9 +16,11 @@ class GenericContainer(Container):
         self.add_block_type("analyzeAudio", AnalyzeAudioBlock)
         self.add_block_type("exportAudio", ExportAudioBlock)
         self.add_block_type("exportAudioSpectrogram", ExportAudioSpectrogramBlock)
+        self.add_block_type("exportma3", ExportMA3Block)
 
         self.add_block("loadAudio")
         self.add_block("transformAudio")
         self.add_block("analyzeAudio")
         self.add_block("exportAudio")
         self.add_block("exportAudioSpectrogram")
+        self.add_block("exportma3")
