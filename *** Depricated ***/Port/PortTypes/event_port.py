c4dc5f9305f06587de909c29628df8ba34de1907
Griffin D <gdennen@G-MBP.local>
2024-12-14 09:45:50 -0800

    Alpha1.0

--- *** Depricated ***/Port/PortTypes/event_port.py
+++
@@ -1,9 +0,0 @@
-from ..port import Port
-from Data.Types.event_data import EventData
-
-class EventPort(Port):
-    name = "EventPort"
-    def __init__(self):
-        super().__init__()
-        self.attribute.set("name", "EventData") # maybe unnecessary now?
-        self.attribute.set("data_type", "EventData")
